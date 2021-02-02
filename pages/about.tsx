4580147cd00dcbe8410b1d9e39b1eb02d877e10e
Lucas <lucasmatosbognotti@gmail.com>
2021-02-02 09:39:21 -0300

    Feat: Creiação da pagina About

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        About !!!!!
+      </h1>
+    </div>
+  );
+}
