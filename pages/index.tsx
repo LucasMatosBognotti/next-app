f20a377cfe67ba3b03b8c3f686fd5cb66b9f324e
Lucas <lucasmatosbognotti@gmail.com>
2021-02-18 21:26:46 -0300

    Agora sim

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,10 +19,6 @@ export default function Home() {
         </p>
 
         <p className={styles.description}>
-          01001100 01110101 01100011 01110011 01100001
-        </p>
-
-        <p className={styles.description}>
           01001100 01110101 01100011 01100001 01110011
         </p>
       </main>
